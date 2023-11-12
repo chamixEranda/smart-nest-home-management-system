e070a3f9401f8ae9945faa21045fd917cd72b550
Chamith Eranda <chamitheranda00@gmail.com>
2023-11-12 20:57:00 +0530

    models & migrations & routes

--- app/Models/SubscriptionPayment.php
+++ app/Models/SubscriptionPayment.php
@@ -9,6 +9,8 @@ class SubscriptionPayment extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'subscription_id', 'amount'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
